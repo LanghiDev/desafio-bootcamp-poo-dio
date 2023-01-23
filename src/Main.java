4e15504967466e7c731b2fcc6b67072bf950189b
LanghiDev <nicolas.langhi@etec.sp.gov.br>
2023-01-23 17:21:15 -0300

    Mentoria e Curso adicionados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.langhidev.desafio.dominio.Curso;
+import br.com.langhidev.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso cursoJava = new Curso();
+
+        cursoJava.setTitulo("Introdução à Linguagem Java");
+        cursoJava.setDescricao("Aprenda os conceitos básicos da linguagem de programação Java");
+        cursoJava.setCargaHoraria(4);
+
+        Curso cursoJS = new Curso("Introdução à Linguagem JavaScript", "Aprenda a dominar JavaScript e crie sites únicos", 4);
+
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria de Java");
+        mentoriaJava.setDescricao("Tire suas dúvidas conosco nesta mentoria explicativa sobre Java");
+        mentoriaJava.setData(LocalDate.now());
+
+        System.out.println(cursoJava);
+        System.out.println(cursoJS);
+        System.out.println(mentoriaJava);
     }
 }
